7d50b9dec248ba234405b50e5d7360f25f159bdb
Schischu <schischu65@gmail.com>
2016-04-26 21:39:14 +0200

    Update Makefiles

--- wingray-vendor.mk
+++ wingray-vendor.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2011 The Android Open Source Project
+# Copyright (C) 2015 Schischu
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,3 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+DEVICE_PACKAGE_OVERLAYS := vendor/nvidia/wingray/overlay
+
+$(call inherit-product, vendor/nvidia/wingray/wingray-vendor-blobs.mk)
